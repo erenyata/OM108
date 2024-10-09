22c7b482bbb459d06d39df2e97c390bee685d9b5
= <erenayata2@gmail.com>
2024-10-09 14:24:12 +0300

    hipotenüs programına alan hesabı eklendi 3.hafta

--- hipotenus.f90
+++ hipotenus.f90
@@ -1,12 +1,14 @@
 PROGRAM HIPO
 IMPLICIT NONE
 
-REAL :: k1,k2,h
+REAL :: k1,k2,h,area
 
 PRINT*,"Ucgenin dik kenar uzunluklarini girin: "
 READ*,k1,k2
 
 h = sqrt((k1**2)+(k2**2))
+area = (k1*k2)/2
 
-PRINT*,"Sonuc: " , h
+PRINT*,"Hipotenus: " , h
+PRINT*,"Alan: " , area
 END PROGRAM HIPO
